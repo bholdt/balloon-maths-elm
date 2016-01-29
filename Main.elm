d337cb9e22db203b0f942c37b78d928981e70bca
Bjorn Holdt <bjorn@getconnect.io>
2016-01-29 16:23:01 +1000

    Introduce Question, Player and Balloon model

--- Main.elm
+++ Main.elm
@@ -8,27 +8,34 @@ main =
 
 type State = Start | Play | Finished
 
+type alias Question =
+    { a: Int,
+      b: Int
+    }
+
 type alias Player =
-    { score: Int }
+    { score: Int
+    , question: Question
+    , balloons: List Balloon
+    }
 
 type alias Balloon =
     { x: Float
     , y: Float
-    , value: Int }
+    , value: Int
+    }
 
 type alias Game =
     { state: State
     , player1: Player
     , player2: Player
-    , balloons: List Balloon
     }
 
 model: Game
 model =
     { state = Start
-    , player1 = Player 0
-    , player2 = Player 0
-    , balloons = [ Balloon 0 2 8, Balloon 2 3 4, Balloon 3 2 8]
+    , player1 = Player 0 (Question 3 2) [ Balloon 0 2 8, Balloon 2 3 4, Balloon 3 2 8]
+    , player2 = Player 0 (Question 3 9) [ Balloon 0 2 8, Balloon 2 3 4, Balloon 3 2 8]
     }
 
 
@@ -46,6 +53,6 @@ type Action = Increment | Decrement
 update action model =
   case action of
     Increment ->
-        { model | player1 = Player (model.player1.score + 1) }
+        { model | player1 = Player (model.player1.score + 1) model.player1.question model.player1.balloons }
     Decrement ->
-        { model | player2 = Player (model.player2.score + 2) }
+        { model | player2 = Player (model.player1.score + 1) model.player1.question model.player1.balloons }
